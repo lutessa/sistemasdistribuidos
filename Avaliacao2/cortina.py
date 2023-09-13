1caa4611071110c467ac890ff3d29c9c884781dc
Cássio Keisuke Yamauchi <37905517+Yamigami@users.noreply.github.com>
2023-09-13 20:26:49 -0300

    Added prints and fixed parts of GUI

--- Avaliacao2/cortina.py
+++ Avaliacao2/cortina.py
@@ -8,13 +8,17 @@ LUMINOSIDADE = 0.0
 def on_connect(client, userdata, flags, rc):
     global ESTADO_CORTINA
     print("Connected with result code "+str(rc))
-    client.subscribe("topic/luminosidade")
-    client.subscribe("topic/controle_cortina")
-
+    result, mid = client.subscribe("topic/luminosidade")
+    print("subscribe connection status code is", result)
+    print("subscribe message id is", mid)
+    result, mid = client.subscribe("topic/controle_cortina")
+    print("subscribe connection status code is", result)
+    print("subscribe message id is", mid)
     ESTADO_CORTINA = False
-    client.publish("topic/estado_cortina","FECHADA")
-    
+    info = client.publish("topic/estado_cortina","FECHADA")
     ##TODO: print result code
+    print("publish connection status code is", info.rc)
+    print("publish message id is", info.mid)
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
@@ -28,9 +32,10 @@ def on_message_luminosidade(client, userdata, message):
 
     if(LUMINOSIDADE <= 0.5):
         ESTADO_CORTINA = True
-        client.publish("topic/estado_cortina","ABERTA")
+        info = client.publish("topic/estado_cortina","ABERTA")
         ## TODO: print result code
-
+        print("publish connection status code is", info.rc)
+        print("publish message id is", info.mid)
 
 def on_message_controle_cortina(client, userdata, message):
     global ESTADO_CORTINA
@@ -38,16 +43,17 @@ def on_message_controle_cortina(client, userdata, message):
     msg = str(message.payload.decode("utf-8"))
     if msg == 'abra':
         ESTADO_CORTINA = True
-        client.publish("topic/estado_cortina","ABERTA")
+        info = client.publish("topic/estado_cortina","ABERTA")
         ## TODO: print result code
+        print("publish connection status code is", info.rc)
+        print("publish message id is", info.mid)
     elif msg == 'feche':
         ESTADO_CORTINA = False
-        client.publish("topic/estado_cortina","FECHADA")
+        info = client.publish("topic/estado_cortina","FECHADA")
         ## TODO: print result code
+        print("publish connection status code is", info.rc)
+        print("publish message id is", info.mid)
         
-
-
-
 #Localizacao do MQTT broker , alterar para o endereço do broker
 mqttBroker = "localhost"
 
@@ -59,7 +65,6 @@ client.connect(mqttBroker)
 
 client.on_connect = on_connect
 client.on_message = on_message
-
 
 client.message_callback_add('topic/controle_cortina', on_message_controle_cortina)
 client.message_callback_add('topic/luminosidade', on_message_luminosidade)
--- Avaliacao2/user.py
+++ Avaliacao2/user.py
@@ -28,12 +28,16 @@ def on_message_estado_led(client, userdata, message):
     msg = str(message.payload.decode("utf-8"))
     if msg == 'ligue':
         ESTADO_LED = True
-        client.publish("topic/estado_led","LIGADO")
+        info = client.publish("topic/estado_led","LIGADO")
+        print("publish 'LIGADO' connection status code is", info.rc)
+        print("publish 'LIGADO' message id is", info.mid)
         ## TODO: print result code
     elif msg == 'desligue':
         ESTADO_LED = False
-        client.publish("topic/estado_led","DESLIGADO")
+        info = client.publish("topic/estado_led","DESLIGADO")
         ## TODO: print result code
+        print("publish 'DESLIGADO' connection status code is", info.rc)
+        print("publish 'DESLIGADO' message id is", info.mid)
         
 
 def on_message_luminosidade(client, userdata, message):
@@ -42,23 +46,20 @@ def on_message_luminosidade(client, userdata, message):
     global ESTADO_LED
     print("luminosidade - received message: " ,str(message.payload.decode("utf-8")))
 
-
     LUMINOSIDADE = float(message.payload.decode("utf-8"))
 
     if LUMINOSIDADE < 0.5 and ESTADO_CORTINA:
         ESTADO_LED = True
-        client.publish("topic/estado_led","LIGADO")
+        info = client.publish("topic/estado_led","LIGADO")
         ## TODO: print result code        
-
+        print("publish 'LIGADO' connection status code is", info.rc)
+        print("publish 'LIGADO' message id is", info.mid)
 
 def button_luminosidade_callback():
     client.subscribe("topic/luminosidade")
 
-
-
 def button_led_callback():
     client.subscribe("topic/estado_led")
-
 
 def button_cortina_callback():
     client.subscribe("topic/estado_cortina")
@@ -75,52 +76,54 @@ def abrir_cortina_callback():
 def fechar_cortina_callback():
     client.publish("topic/controle_cortina", "feche")
 
-
 #Localizacao do MQTT broker , alterar para o endereço do broker
 mqttBroker = "localhost"
 
-
 #Configura um novo client, deve possuir um nome unico
 client = mqtt.Client("USER")
-
 
 #Conecta ao Broker, port=1883 por padrão
 client.connect(mqttBroker) 
 client.on_connect = on_connect
 
-
 root = tk.Tk()
+root.geometry("600x400")
 root.title("MQTT")
+root.config(padx=10, pady=20)
+root.configure(bg='gray')
 
+sub_label = tk.Label(root, text="Inscrição em tópicos", font=('Arial 20 bold'), padx=10, pady=10, background='gray')
+sub_label.pack()
 
-button1 = tk.Button(root, text="Sub topico luminosidade", command=button_luminosidade_callback)
+button1 = tk.Button(root, text="Sub topico luminosidade", command=button_luminosidade_callback, activeforeground='red')
 button1.pack()
 
-button2 = tk.Button(root, text="Sub topico led", command=button_led_callback)
+button2 = tk.Button(root, text="Sub topico led", command=button_led_callback, activeforeground='red')
 button2.pack()
 
-button3 = tk.Button(root, text="Sub topico cortina", command=button_cortina_callback)
+button3 = tk.Button(root, text="Sub topico cortina", command=button_cortina_callback, activeforeground='red')
 button3.pack()
 
+led_label = tk.Label(root, text="LED", font=('Arial 20 bold'), padx=10, pady=10, background='gray')
+led_label.pack()
 
-ligar_led = tk.Button(root, text="Ligar", command=ligar_led_callback)
+ligar_led = tk.Button(root, text="Ligar", command=ligar_led_callback, activeforeground='red')
 ligar_led.pack()
 
-desligar_led = tk.Button(root, text="Desligar", command=desligar_led_callback)
+desligar_led = tk.Button(root, text="Desligar", command=desligar_led_callback, activeforeground='red')
 desligar_led.pack()
 
-abrir_cortina = tk.Button(root, text="Abrir", command=abrir_cortina_callback)
+cortina_label = tk.Label(root, text="Cortina", font=('Arial 20 bold'), padx=10, pady=10, background='gray')
+cortina_label.pack()
+
+abrir_cortina = tk.Button(root, text="Abrir", command=abrir_cortina_callback, activeforeground='red')
 abrir_cortina.pack()
 
-fechar_cortina = tk.Button(root, text="Fechar", command=fechar_cortina_callback)
+fechar_cortina = tk.Button(root, text="Fechar", command=fechar_cortina_callback, activeforeground='red')
 fechar_cortina.pack()
 
 root.mainloop()
 
-
-
-
 client.on_message = on_message
-
 
 client.loop_forever()
