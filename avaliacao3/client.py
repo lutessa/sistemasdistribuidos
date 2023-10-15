5d1a2ef8b5604a915954702dc803cf5d6b18ab57
Cassio Yamauchi <cassio.yamauchi@gmail.com>
2023-10-15 19:49:59 -0300

    added GUI

--- avaliacao3/client.py
+++ avaliacao3/client.py
@@ -1,8 +1,114 @@
 import Pyro5.api
+import Pyro5.client
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+import tkinter as tk
+from tkinter import font as tkfont
 import threading 
+
+class UIMainPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Ações Disponíveis", font=controller.title_font)
+        label.grid()
+        insertButton = tk.Button(self, text="Entrada de Produto",
+                            command=lambda: controller.show_frame("InsertPage"))
+        removeButton = tk.Button(self, text="Saída de Produto",
+                            command=lambda: controller.show_frame("RemovePage"))
+        reportButton = tk.Button(self, text="Geração de Relatório",
+                            command=lambda: controller.show_frame("ReportPage"))
+        insertButton.grid(row=1, column=0)
+        removeButton.grid(row=2, column=0)
+        reportButton.grid(row=3, column=0)
+        self.grid_columnconfigure(0, weight=1)
+
+class InsertPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Página de Entrada de Item", font=controller.title_font)
+        label.grid(row = 0, column=1)
+        tk.Label(self, text="Código").grid(row=1, column=0)
+        tk.Label(self, text="Nome").grid(row=2, column=0)
+        tk.Label(self, text="Descrição").grid(row=3, column=0)
+        tk.Label(self, text="Quantidade").grid(row=4, column=0)
+        tk.Label(self, text="Preço unitário").grid(row=5, column=0)
+        tk.Label(self, text="Estoque mínimo").grid(row=6, column=0)
+        codeEntry = tk.Entry(self)
+        nameEntry = tk.Entry(self)
+        descriptionEntry = tk.Entry(self)
+        quantityEntry = tk.Entry(self)
+        priceEntry = tk.Entry(self)
+        minStorageEntry = tk.Entry(self)
+        codeEntry.grid(row=1, column=1)
+        nameEntry.grid(row=2, column=1)
+        descriptionEntry.grid(row=3, column=1)
+        quantityEntry.grid(row=4, column=1)
+        priceEntry.grid(row=5, column=1)
+        minStorageEntry.grid(row=6, column=1)
+        button = tk.Button(self, text="Insere Item",
+                           command=client.insertItem)
+        button.grid(row=7, column=1)
+        button = tk.Button(self, text="Returna à Página Principal",
+                           command=lambda: controller.show_frame("UIMainPage"))
+        button.grid(row=8, column=1)
+
+class RemovePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Página de Saída de Item", font=controller.title_font)
+        label.grid(row = 0, column=1)
+        tk.Label(self, text="Código").grid(row=1, column=0)
+        codeEntry = tk.Entry(self)
+        codeEntry.grid(row=1, column=1)
+        tk.Label(self, text="Quantidade").grid(row=2, column=0)
+        quantityEntry = tk.Entry(self)
+        quantityEntry.grid(row=2, column=1)
+        button = tk.Button(self, text="Remove Item",
+                           command=client.removeItem)
+        button.grid(row=3, column=1)
+        button = tk.Button(self, text="Returna à Página Principal",
+                           command=lambda: controller.show_frame("UIMainPage"))
+        button.grid(row=4, column=1)
+    
+class ReportPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Página de Geração de Relatório", font=controller.title_font)
+        label.grid(row = 0, column=1)
+        button = tk.Button(self, text="Returna à Página Principal",
+                           command=lambda: controller.show_frame("UIMainPage"))
+        button.grid(row=3, column=1)
+
+class graphics(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (UIMainPage, InsertPage, RemovePage, ReportPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("UIMainPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 class client(object):
     def __init__(self, manager_uri):
@@ -16,12 +122,30 @@ class client(object):
     @Pyro5.api.expose
     @Pyro5.api.callback
     def min_stock(self, item):
-        print(item)
+        print('item', item, 'is missing')
 
     @Pyro5.api.expose
     @Pyro5.api.callback
     def not_sold_report(self, items):
         print(items)
+
+    def insertItem():
+        # nameServer = Pyro5.core.locate_ns()
+        # management_uri = nameServer.lookup("Management")
+        # managementServer = Pyro5.api.Proxy(management_uri)
+        nameServer = Pyro5.api.locate_ns()
+        management_uri = "PYRO:Pyro.NameServer@172.31.60.185:9090"
+        managementProxy = Pyro5.api.Proxy(management_uri)
+        # managementProxy = Pyro5.api.Proxy("PYRONAME:Management")
+        managementProxy.inserItem()
+        pass
+
+    def removeItem(self):
+        nameServer = Pyro5.core.locate_ns()
+        management_uri = nameServer.lookup("Management")
+        managementServer = Pyro5.api.Proxy(management_uri)
+        managementServer.removeItem()
+        pass
 
 
 def create_pair_key():
@@ -36,3 +160,6 @@ def create_pair_key():
         pu.write(public_pem)
 
 
+
+UI = graphics()
+UI.mainloop()
--- avaliacao3/management.py
+++ avaliacao3/management.py
@@ -1,7 +1,27 @@
+import threading
+import socket
 import Pyro5.api
+import Pyro5.socketutil
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+
+class NameServer(object):
+    def __init__(self):
+        self.hostname = socket.gethostname()
+        self.ip = Pyro5.socketutil.get_ip_address(None, workaround127=True)
+        # start a name server with broadcast server
+        self.nameserverUri, self.nameserverDaemon, self.broadcastServer = Pyro5.nameserver.start_ns(host=self.ip)
+        assert self.broadcastServer is not None, "expect a broadcast server to be created"
+        print("got a Nameserver, uri=%s" % self.nameserverUri)
+    def initDaemon(self): 
+        # create a Pyro daemon
+        pyrodaemon = Pyro5.api.Daemon(host=self.hostname)
+        serveruri = pyrodaemon.register(Management)
+        print("server uri=%s" % serveruri)
+        # register it with the nameserver
+        self.nameserverDaemon.nameserver.register("Management", serveruri)
+        self.nameserverDaemon.requestLoop()
 
 class User():
     def __init__(self, name, pub_key, remote_obj):
@@ -14,10 +34,12 @@ class Management(object):
         self.estoque = {}
         self.users = []
     @Pyro5.api.expose
-    def register(self, name, pubKey, uri):
+    def register(self, name, pub_key, uri):
         user_proxy = Pyro5.api.Proxy(uri)
         ### if user name pub ! exists
-        self.users.append(User(name,pub_key,user_proxy ))
+        for user in self.users:
+            if(User.name != user.name and User.pub_key != user.pub_key and User.user_proxy != user.user_proxy):
+                self.users.append(User(name,pub_key,user_proxy))
     def checkKey(self):
         pass
     #TODO: tratar entrada no cliente
@@ -30,11 +52,13 @@ class Management(object):
             self.estoque[code]["name"] = name
             self.estoque[code]["description"] = description
             self.estoque[code]["price"] = price
+            self.estoque[code]["minStorage"] = minStorage
             self.estoque[code]["qnt"] += qnt
         else:
             self.estoque[code]["name"] = name
             self.estoque[code]["description"] = description
             self.estoque[code]["price"] = price
+            self.estoque[code]["minStorage"] = minStorage
             self.estoque[code]["qtn"] = qnt
 
         return "Success"
@@ -45,26 +69,32 @@ class Management(object):
             return "ERROR"
         
         if code in self.estoque:
+            #if amount to remove is bigger than existing
             if qnt > self.estoque[code]["qnt"]:
+                print('O estoque é insuficiente para ser retirada a quantidade pedida')
+                return "error"
+            if self.estoque[code]["qnt"]-qnt < self.estoque[code]["minStorage"]:
                 #TODO: eniar flag estoque min p client
-                for user in users:
+                for user in self.users:
                     user.remote_obj.min_stock(self.estoque[code]["name"])
                 return "error"
             self.estoque[code]["qnt"] -= qnt
+        else:
+            print('O item requerido não está disponível')
+
     def getReport(self):
-        pass
-
-
-    def get_fortune(self, name):
-        return "Hello, {0}. Here is your fortune message:\n" \
-               "Tomorrow's lucky number is 12345678.".format(name)
+        threading.Timer(10, self.getReport).start()
+        
 
 if __name__ == "__main__":
 
-    daemon = Pyro5.server.Daemon()         # make a Pyro daemon
-    ns = Pyro5.api.locate_ns()             # find the name server
-    uri = daemon.register(Management)   # register the greeting maker as a Pyro object
-ns.register("example.greeting", uri)   # register the object with a name in the name server
+    # daemon = Pyro5.server.Daemon()         # make a Pyro daemon
+    # ns = Pyro5.api.locate_ns()             # find the name server
+    # uri = daemon.register(Management)   # register the greeting maker as a Pyro object
+
+# ns.register("example.greeting", uri)   # register the object with a name in the name server
+    managementServer = NameServer()
+    managementServer.initDaemon()
 
 print("Ready.")
-daemon.requestLoop()   
+# daemon.requestLoop()   
