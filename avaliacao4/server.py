cc6645b21b0d792f3fb25161cab0719171c7bbac
lutessa <raissa.higa@gmail.com>
2023-11-09 15:55:14 -0300

    server fix

--- avaliacao4/server.py
+++ avaliacao4/server.py
@@ -1,13 +1,12 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify, request
 from flask_sse import sse
 import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask_cors import CORS
 import datetime
 from helper import get_data,get_schd_time
-
-i = 0
-
+from datetime import datetime
+estoque = {}
 app = Flask(__name__)
 CORS(app)
 #CORS(app, resources={"/events": {"origins": "file://"}})
@@ -29,18 +28,73 @@ def server_side_event():
         print("Event Scheduled at ",datetime.datetime.now())
 
 
-sched = BackgroundScheduler(daemon=True)
-sched.add_job(server_side_event,'interval',seconds=get_schd_time())
-sched.start()
+# sched = BackgroundScheduler(daemon=True)
+# sched.add_job(server_side_event,'interval',seconds=get_schd_time())
+# sched.start()
+
+@app.route('/insert', methods=["POST"])
+def insert_item():
+    data = request.get_json()
+    
+    current_time = datetime.now()
+
+    code = data['code']
+    name = data["name"]
+    description = data["description"]
+    price = data["price"]
+    minStorage = data['minStorage']
+    qnt = data['qnt']
 
 
-# @app.route('/')
-# def index():
-#     global i
-#     i+=1
-#     d = {'i':i}
-#     #return jsonify(d)
-#     return jsonify(get_data())
+    if code in estoque:
+        estoque[code]["name"] = name
+        estoque[code]["description"] = description
+        estoque[code]["price"] = price
+        estoque[code]["minStorage"] = minStorage
+        estoque[code]["qnt"] += qnt
+        estoque[code]["last_insert_time"] = current_time
+
+        return "Objeto inserido com sucesso"
+    else:
+        estoque[code] = {
+                            "name": name,
+                            "description": description,
+                            "price": price,
+                            "minStorage": minStorage,
+                            "qnt": qnt,
+                            "last_insert_time": current_time,
+                            "last_remove_time": datetime.fromtimestamp(0)
+                        }
+        return "Novo objeto adicionado com sucesso"
+
+    #return "OK"
+
+@app.route('/remove', methods=["POST"])
+def remove_item():
+    data = request.get_json()
+
+    current_time = datetime.now()
+
+    code = data['code']
+    qnt = data['qnt']
+
+    if code in estoque:
+        #if amount to remove is bigger than existing
+        if qnt > estoque[code]["qnt"]:
+            print('O estoque é insuficiente para ser retirada a quantidade pedida')
+            return "ERROR: O estoque é insuficiente para ser retirada a quantidade pedida"
+        if estoque[code]["qnt"]-qnt < estoque[code]["minStorage"]:
+            print("sending flag")
+            sse.publish(f"Estoque minimo de {estoque[code]['name']} atingido", type='dataUpdate')
+            estoque[code]["last_remove_time"] = current_time
+            return "1: Operação concluída, objeto no limite"
+        else:
+            print('O item requerido não está disponível')
+            return 'ERROR: O item requerido não está disponível'
+    
+
+    #return "OK"
+
 
 
 if __name__ == '__main__':
